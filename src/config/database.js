f78b9409410cc86dbec4abba78c708e2bb8e009b
wilderOliveira92 <wilder.oliveira92@gmail.com>
2020-02-03 18:38:33 -0300

    chore - configuration

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'postgres',
+    host: localhost, //"192.168.99.100",
+    username: 'postgres',
+    password: 'fastfeet',
+    database: 'fastfeet',
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+    },
+};
