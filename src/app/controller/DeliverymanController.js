409e7ecdb143035c242585a0908bf0d6b9019cdf
wilderOliveira92 <wilder.oliveira92@gmail.com>
2020-02-10 19:31:51 -0300

    create deliveryman

---
+++ src/app/controller/DeliverymanController.js
@@ -0,0 +1,11 @@
+class DeliverymanController {
+    async store(req, res) {}
+
+    async index(req, res) {}
+
+    async update(req, res) {}
+
+    async delete(req, res) {}
+}
+
+export default new DeliverymanController();
